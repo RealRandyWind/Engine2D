c1f0f6bbbbd0ac1743304935aaf8a83ddf150364
Randy Wind <randywind@live.nl>
2014-11-14 15:23:06 +0100

    very mesy.

---
+++ scripts/engine/Set.js
@@ -0,0 +1,38 @@
+ENGINE2D.Set = function () {
+	this.elements = undefined;
+};
+/* TODO Implement iterator if we use
+ * no duplicates, comparable
+ * element -> (value,*Compare(x,y))
+ */
+ENGINE2D.Set.prototype = {
+
+	constructor: ENGINE2D.Set,
+
+	Insert: function (elememt) {
+		/* TODO */
+		console.error('_ERROR: [Set.Insert] function not yet implemented');
+	},
+
+	Remove: function (elememt) {
+		/* TODO */
+		console.error('_ERROR: [Set.Remove] function not yet implemented');
+	},
+
+	Union: function (set) {
+		/* TODO */
+		console.error('_ERROR: [Set.Union] function not yet implemented');
+	},
+
+	Intersect: function (set) {
+		/* TODO */
+		console.error('_ERROR: [Set.Intersect] function not yet implemented');
+	},
+
+	Difference: function (set) {
+		/* TODO */
+		console.error('_ERROR: [Set.Difference] function not yet implemented');
+	}
+};
+
+
